866a204e6ca8fe4542b60a7bacdf32e5d4fc50be
prebuilds <hgstyle-yt@hotmail.com>
2025-03-15 16:09:35 +0100

    Update gotosrc.sh

--- gotosrc.sh
+++ gotosrc.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# DO NOT use this tool
+# it is here as a reference, a note, not to be ran
+exit
+
 # Go to the source code directory
 cd /prefix/src
 while [ "$(ls -1 | wc -l)" -eq 1 ]; do

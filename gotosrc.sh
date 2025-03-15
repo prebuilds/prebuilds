6d9dda2215b8c4c2ff11d4e0a2375407b741e4cf
prebuilds <hgstyle-yt@hotmail.com>
2025-03-15 15:47:35 +0100

    Add debug

--- gotosrc.sh
+++ gotosrc.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+echo Gotosrc.SH debug before: $(pwd)
+
 # Go to the source code directory
 cd /prefix/src
 while [ "$(ls -1 | wc -l)" -eq 1 ]; do
     cd "$(ls -1)" || break
 done
+
+echo Gotosrc.SH debug after: $(pwd)

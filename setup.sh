9475a34529cf722b13f6e5f01e6476cc25190445
prebuilds <hgstyle-yt@hotmail.com>
2025-03-15 12:33:54 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Update sources
+sudo apt update
+
+# Upgrade software
+sudo apt upgrade -y
+
+# Install needed software
+for pkgname in "$@"; do
+  case "$pkgname" in
+    "7z") sudo apt install p7zip -y ;;
+    "git") sudo apt install git -y ;;
+    "wget") sudo apt install wget -y ;;
+    "gcc") sudo apt install gcc -y ;;
+    "g++") sudo apt install g++ -y ;;
+    "libc6") sudo apt install libc6 -y ;;
+    "libstdc++6") sudo apt install libstdc++6 -y ;;
+    "binutils") sudo apt install binutils -y ;;
+    "make") sudo apt install make -y ;;
+  esac
+done
